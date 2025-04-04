f31ccc94fe0a631d6942228337c874a265060ef2
KASSIO DE ARRUDA AGUIAR <202503056986@alunos.unifanor.edu.br>
2025-04-04 08:51:25 -0300

    Exercicios

---
+++ Exercicios.py
@@ -0,0 +1,14 @@
+#Condicional simples
+x= 10
+y= 10
+if x == y:
+    print("x é igual a y")
+
+#Condicional composta
+x1=6
+y1=10
+if x1 == y1:
+    print("x1 é igual a y1")
+else:
+    print("x1 não é igual a y1")
+
