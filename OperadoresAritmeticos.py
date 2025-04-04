50d1b4d7bdec86ef4af8c7d8a7e99ca0f845cbf9
KASSIO DE ARRUDA AGUIAR <202503056986@alunos.unifanor.edu.br>
2025-04-04 07:28:24 -0300

    Operadores

---
+++ OperadoresAritmeticos.py
@@ -0,0 +1,39 @@
+# Adição
+numero1 =10
+numero2 =20
+
+soma = numero1 + numero2 
+print(soma)
+
+#Subtracao
+
+subtracao = numero1 - numero2 
+print(subtracao)
+
+# Multiplicação 
+
+multiplicaçao = numero1 * numero2 
+print(multiplicaçao)
+
+# Divisão 
+
+divisao = numero1 / numero2
+print(divisao)
+
+# Potencia 
+
+potencia = numero1 ** numero2
+print(potencia)
+
+# Modulo
+
+modulo = numero1 % numero2
+print(modulo)
+
+# Resto da Divisão 
+
+restodadivisao = numero1 // numero2
+print(restodadivisao)
+
+
+print("Kassio")
